8bc2ca343db4a921b179144f8c1547e6cc46e2d8
larell mcbride <mcbridelarell@gmail.com>
2020-08-13 13:24:35 -0400

    Frontend template initial commit

---
+++ src/views/Index.js
@@ -0,0 +1,276 @@
+/*!
+
+=========================================================
+* Argon Dashboard React - v1.1.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-dashboard-react
+* Copyright 2019 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+// node.js library that concatenates classes (strings)
+import classnames from "classnames";
+// javascipt plugin for creating charts
+import Chart from "chart.js";
+// react plugin used to create charts
+import { Line, Bar } from "react-chartjs-2";
+// reactstrap components
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  NavItem,
+  NavLink,
+  Nav,
+  Progress,
+  Table,
+  Container,
+  Row,
+  Col
+} from "reactstrap";
+
+// core components
+import {
+  chartOptions,
+  parseOptions,
+  chartExample1,
+  chartExample2
+} from "variables/charts.js";
+
+import Header from "components/Headers/Header.js";
+
+class Index extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      activeNav: 1,
+      chartExample1Data: "data1"
+    };
+    if (window.Chart) {
+      parseOptions(Chart, chartOptions());
+    }
+  }
+  toggleNavs = (e, index) => {
+    e.preventDefault();
+    this.setState({
+      activeNav: index,
+      chartExample1Data:
+        this.state.chartExample1Data === "data1" ? "data2" : "data1"
+    });
+  };
+  render() {
+    return (
+      <>
+        <Header />
+        {/* Page content */}
+        <Container className="mt--7" fluid>
+          <Row>
+            <Col className="mb-5 mb-xl-0" xl="8">
+              <Card className="bg-gradient-default shadow">
+                <CardHeader className="bg-transparent">
+                  <Row className="align-items-center">
+                    <div className="col">
+                      <h6 className="text-uppercase text-light ls-1 mb-1">
+                        
+                      </h6>
+                      <h2 className="text-white mb-0">Here6</h2>
+                    </div>
+                    <div className="col">
+                      <Nav className="justify-content-end" pills>
+                        
+                        
+                      </Nav>
+                    </div>
+                  </Row>
+                </CardHeader>
+                <CardBody>
+                  {/* Chart */}
+                  <div className="chart">
+                    game?
+                  </div>
+                </CardBody>
+              </Card>
+            </Col>
+            <Col xl="4">
+              <Card className="shadow">
+                <CardHeader className="bg-transparent">
+                  <Row className="align-items-center">
+                    <div className="col">
+                      <h6 className="text-uppercase text-muted ls-1 mb-1">
+                        here7
+                      </h6>
+                      <h2 className="mb-0"></h2>
+                    </div>
+                  </Row>
+                </CardHeader>
+                <CardBody>
+                  {/* Chart */}
+                  <div className="chart">
+                    
+                  </div>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+          <Row className="mt-5">
+            <Col className="mb-5 mb-xl-0" xl="8">
+              <Card className="shadow">
+                <CardHeader className="border-0">
+                  <Row className="align-items-center">
+                    <div className="col">
+                      <h3 className="mb-0"></h3>
+                    </div>
+                    <div className="col text-right">
+                      
+                        
+                      
+                    </div>
+                  </Row>
+                </CardHeader>
+                <Table className="align-items-center table-flush" responsive>
+                  <thead className="thead-light">
+                    here8
+                  </thead>
+                  <tbody>
+                    <tr>
+                      
+                    </tr>
+                    
+                    <tr>
+                      
+                    </tr>
+                    <tr>
+                      
+                    </tr>
+                    <tr>
+                      
+                    </tr>
+                  </tbody>
+                </Table>
+              </Card>
+            </Col>
+            <Col xl="4">
+              <Card className="shadow">
+                <CardHeader className="border-0">
+                  <Row className="align-items-center">
+                    <div className="col">
+                      <h3 className="mb-0">Social traffic</h3>
+                    </div>
+                    <div className="col text-right">
+                      <Button
+                        color="primary"
+                        href="#pablo"
+                        onClick={e => e.preventDefault()}
+                        size="sm"
+                      >
+                        See all
+                      </Button>
+                    </div>
+                  </Row>
+                </CardHeader>
+                <Table className="align-items-center table-flush" responsive>
+                  <thead className="thead-light">
+                    <tr>
+                      <th scope="col">Referral</th>
+                      <th scope="col">Visitors</th>
+                      <th scope="col" />
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <th scope="row">Facebook</th>
+                      <td>1,480</td>
+                      <td>
+                        <div className="d-flex align-items-center">
+                          <span className="mr-2">60%</span>
+                          <div>
+                            <Progress
+                              max="100"
+                              value="60"
+                              barClassName="bg-gradient-danger"
+                            />
+                          </div>
+                        </div>
+                      </td>
+                    </tr>
+                    <tr>
+                      <th scope="row">Facebook</th>
+                      <td>5,480</td>
+                      <td>
+                        <div className="d-flex align-items-center">
+                          <span className="mr-2">70%</span>
+                          <div>
+                            <Progress
+                              max="100"
+                              value="70"
+                              barClassName="bg-gradient-success"
+                            />
+                          </div>
+                        </div>
+                      </td>
+                    </tr>
+                    <tr>
+                      <th scope="row">Google</th>
+                      <td>4,807</td>
+                      <td>
+                        <div className="d-flex align-items-center">
+                          <span className="mr-2">80%</span>
+                          <div>
+                            <Progress max="100" value="80" />
+                          </div>
+                        </div>
+                      </td>
+                    </tr>
+                    <tr>
+                      <th scope="row">Instagram</th>
+                      <td>3,678</td>
+                      <td>
+                        <div className="d-flex align-items-center">
+                          <span className="mr-2">75%</span>
+                          <div>
+                            <Progress
+                              max="100"
+                              value="75"
+                              barClassName="bg-gradient-info"
+                            />
+                          </div>
+                        </div>
+                      </td>
+                    </tr>
+                    <tr>
+                      <th scope="row">twitter</th>
+                      <td>2,645</td>
+                      <td>
+                        <div className="d-flex align-items-center">
+                          <span className="mr-2">30%</span>
+                          <div>
+                            <Progress
+                              max="100"
+                              value="30"
+                              barClassName="bg-gradient-warning"
+                            />
+                          </div>
+                        </div>
+                      </td>
+                    </tr>
+                  </tbody>
+                </Table>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </>
+    );
+  }
+}
+
+export default Index;
